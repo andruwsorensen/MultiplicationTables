350c550fedb40127200c28dc750fe2fed0838832
andruw-sorensen <andruw.sorensen@gmail.com>
2024-02-09 15:13:26 -0700

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Andruw Sorensen on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
